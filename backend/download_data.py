7aa61f50d50e1cee453142f846d550c4d7a50dde
harikrishna2301 <harikrishnabhyrraju22@gmail.com>
2026-03-18 01:04:27 +0530

    Fix corrupted model overwrite

--- backend/download_data.py
+++ backend/download_data.py
@@ -3,42 +3,37 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-files = {
-    os.path.join(BASE_DIR, "data/final_dataset.csv"):
-    "https://drive.google.com/uc?export=download&id=1ijuOLQx-1vgS2kHvkIZ_00apNNtfav7L",
+def download_file(file_id, destination):
+    URL = "https://drive.google.com/uc?export=download"
 
-    os.path.join(BASE_DIR, "models/traffic_model.pkl"):
-    "https://drive.google.com/uc?export=download&id=1nzwfUSxRU54csv5Qlu8hbQMWBGGvvv4g",
+    session = requests.Session()
 
-    os.path.join(BASE_DIR, "models/pollution_model.pkl"):
-    "https://drive.google.com/uc?export=download&id=1JwNLPW3Wf4n_qShj84N3TjIYAaZsFzBe"
-}
+    response = session.get(URL, params={"id": file_id}, stream=True)
 
-def download_file(path, url):
-    try:
-        print(f"📥 Downloading {path}...")
+    # Handle large file confirmation
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            params = {"id": file_id, "confirm": value}
+            response = session.get(URL, params=params, stream=True)
+            break
 
-        response = requests.get(url, stream=True, timeout=60)
+    if os.path.exists(destination):
+        os.remove(destination)
 
-        if response.status_code != 200:
-            raise Exception(f"Failed to download {path}")
+    # Save file
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(32768):
+            if chunk:
+                f.write(chunk)
 
-        with open(path, "wb") as f:
-            for chunk in response.iter_content(8192):
-                if chunk:
-                    f.write(chunk)
-
-        print(f"✅ Downloaded {path}")
-
-    except Exception as e:
-        print(f"❌ Error downloading {path}: {e}")
-        raise e
+    print(f"✅ Downloaded {destination}")
 
 
-for path, url in files.items():
-    os.makedirs(os.path.dirname(path), exist_ok=True)
+# create folders
+os.makedirs(os.path.join(BASE_DIR, "models"), exist_ok=True)
+os.makedirs(os.path.join(BASE_DIR, "data"), exist_ok=True)
 
-    if not os.path.exists(path):
-        download_file(path, url)
-    else:
-        print(f"✔ Already exists: {path}")
+# download files
+download_file("1nzwfUSxRU54csv5Qlu8hbQMWBGGvvv4g", os.path.join(BASE_DIR, "models/traffic_model.pkl"))
+download_file("1JwNLPW3Wf4n_qShj84N3TjIYAaZsFzBe", os.path.join(BASE_DIR, "models/pollution_model.pkl"))
+download_file("1ijuOLQx-1vgS2kHvkIZ_00apNNtfav7L", os.path.join(BASE_DIR, "data/final_dataset.csv"))
