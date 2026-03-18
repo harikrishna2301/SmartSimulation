ca1bee31a9a8659b4952cf169389d2b568f96e91
harikrishna2301 <harikrishnabhyrraju22@gmail.com>
2026-03-18 16:21:13 +0530

    Fix model download using gdown

--- backend/download_data.py
+++ backend/download_data.py
@@ -1,39 +1,29 @@
-import requests
+import gdown
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def download_file(file_id, destination):
-    URL = "https://drive.google.com/uc?export=download"
+files = {
+    "models/traffic_model.pkl": "1nzwfUSxRU54csv5Qlu8hbQMWBGGvvv4g",
+    "models/pollution_model.pkl": "1JwNLPW3Wf4n_qShj84N3TjIYAaZsFzBe",
+    "data/final_dataset.csv": "1ijuOLQx-1vgS2kHvkIZ_00apNNtfav7L"
+}
 
-    session = requests.Session()
+for path, file_id in files.items():
+    full_path = os.path.join(BASE_DIR, path)
 
-    response = session.get(URL, params={"id": file_id}, stream=True)
+    os.makedirs(os.path.dirname(full_path), exist_ok=True)
 
-    # Handle large file confirmation
-    for key, value in response.cookies.items():
-        if key.startswith("download_warning"):
-            params = {"id": file_id, "confirm": value}
-            response = session.get(URL, params=params, stream=True)
-            break
+    # delete old corrupted file
+    if os.path.exists(full_path):
+        os.remove(full_path)
 
-    if os.path.exists(destination):
-        os.remove(destination)
+    print(f"⬇ Downloading {path}...")
 
-    # Save file
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(32768):
-            if chunk:
-                f.write(chunk)
+    gdown.download(
+        id=file_id,
+        output=full_path,
+        quiet=False
+    )
 
-    print(f"✅ Downloaded {destination}")
-
-
-# create folders
-os.makedirs(os.path.join(BASE_DIR, "models"), exist_ok=True)
-os.makedirs(os.path.join(BASE_DIR, "data"), exist_ok=True)
-
-# download files
-download_file("1nzwfUSxRU54csv5Qlu8hbQMWBGGvvv4g", os.path.join(BASE_DIR, "models/traffic_model.pkl"))
-download_file("1JwNLPW3Wf4n_qShj84N3TjIYAaZsFzBe", os.path.join(BASE_DIR, "models/pollution_model.pkl"))
-download_file("1ijuOLQx-1vgS2kHvkIZ_00apNNtfav7L", os.path.join(BASE_DIR, "data/final_dataset.csv"))
+    print(f"✅ Downloaded {path}")
